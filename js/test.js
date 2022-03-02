51860a9d57df5249f66fa9c86b5c93c21f1da713
ahn0ng <71095151+ahn0ng@users.noreply.github.com>
2022-03-02 14:56:28 +0900

    Add files via upload

---
+++ js/test.js
@@ -0,0 +1,18 @@
+$(function(){
+
+    // var screen_width;
+
+    // screen_width =$(window).width();
+    // var main_width = screen_width-180;
+
+    // $("#main").css("width",main_width);
+
+
+    // 그리드 레이아웃
+    $('.grid').masonry({
+    // options
+        itemSelector: '.item',
+    });
+});
+
+
